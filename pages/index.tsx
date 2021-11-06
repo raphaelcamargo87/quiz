33ae0f90b7424d30015be8747d8f2495e9964479
Raphael Camargo <raphaelcamargo87@gmail.com>
2021-11-05 22:05:19 -0300

    Alterado URL da API

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Questionario from "../components/Questionario";
 import QuestaoModel from "../model/questao";
 import { useRouter } from "next/router";
 
-const BASE_URL = "http://localhost:3000/api";
+// const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "/api";
 
 export default function Home() {
     const router = useRouter();
